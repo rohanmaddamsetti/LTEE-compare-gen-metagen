3a8c4b47d649ad712c7f74f89a5e8ebc24587fc6
Rohan Maddamsetti <rohan.maddamsetti@gmail.com>
2022-02-11 12:51:19 -0500

    SLiM code is looking good

--- SLiM-to-STIMS.R
+++ SLiM-to-STIMS.R
@@ -3,15 +3,15 @@
 ## take the SliM output file and convert it into input for STIMS.
 
 library(tidyverse)
+library(data.table)
 
-## GLOBAL VARIABLES
-freq_threshold <- 0.01
-Ne <- 1e4##1e7
 
-## This file reflects all mutations that were segregating
-## in the population as were sampled every 100 
-## generations.
-d <- read.csv("../results/SLiM-results/SLiM-output.txt", header = F, sep = " ") %>%
+## This function nicely formats all mutations over time into a dataframe.
+## Further processing is needed to generate STIMS input.
+SLiM.output.to.dataframe <- function(SLiM.outfile, freq_threshold=0.01, Ne=1e5) {
+    ## all mutations in the population, sampled every 100 generations.
+    SLiM.outfile %>%
+    data.table::fread(header = F, sep = " ") %>%
     ## Remove unnecessary columns from SLiM output. 
     select(c("V2", "V5" , "V6", "V7", "V10", "V11", "V12")) %>%
     rename(Generation = V2) %>%
@@ -37,13 +37,25 @@ d <- read.csv("../results/SLiM-results/SLiM-output.txt", header = F, sep = " ") 
     mutate("allele_freq" = prevalence/Ne) %>%
     ## Filter mutations with allele frequencies above the sampling threshold.
     filter(allele_freq > freq_threshold) %>%
-    ## IMPORTANT: There can only be one row per mutation.
-    group_by(ID, Annotation, Position, Population, t0) %>%
-    arrange(Generation) %>%
-    ## only take the first row (the first generation at which allele_freq > threshold)
-    filter(row_number() == 1) %>%
-    arrange(ID) ## to double-check that each ID is unique
+    arrange(ID, Generation) ## arrange each mutation by generation.
+}
 
+## This function makes STIMS input-- only one mutation per row,
+## representing the first time the mutation was observed in the population.
+SLiM.output.to.STIMS.input <- function(SLiM.output, freq_threshold=0.01, Ne=1e5) {
+    SLiM.output.to.dataframe(SLiM.output, freq_threshold, Ne) %>%
+        ## IMPORTANT: There can only be one row per mutation.
+        group_by(ID, Annotation, Position, Population, t0) %>%
+        arrange(Generation) %>%
+        ## only take the first row (the first generation at which allele_freq > threshold)
+        filter(row_number() == 1) %>%
+        arrange(ID) ## to double-check that each ID is unique    
+}
+
+
+d <- SLiM.output.to.STIMS.input(
+    "../results/SLiM-results/SLiMoutput_Ne10000_mu10-7_numgens5000.txt",
+    freq_threshold = 0.01, Ne = 1e5)
 
 ## write out the STIMS input file.
 write.csv(d, "../results/SLiM-results/SLiM-5000gen-OnePercent-Hypermutator.csv",
