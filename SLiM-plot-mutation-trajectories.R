57e45c8c19d847d182b1f3a83ae72cacb11b7b15
Rohan Maddamsetti <rohan.maddamsetti@gmail.com>
2022-02-12 19:02:59 -0500

    found and fixed the bug

--- SLiM-plot-mutation-trajectories.R
+++ SLiM-plot-mutation-trajectories.R
@@ -7,36 +7,44 @@ library(cowplot)
 library(gghighlight)
 
 
+## IMPORTANT: we are assuming that each gene is 1000bp long.
+annotate.Gene.per.mutation <- function(df, gene.length = 1000) {
+    ## annotate the Gene for each mutation.
+    df %>%
+        mutate(GeneBin = trunc(Position/gene.length)+1) %>% 
+        mutate(Gene = paste("g", GeneBin, sep = "")) %>% 
+        select(-GeneBin)
+}
+
+
 SLiM.output.to.dataframe <- function(SLiM.outfile, pop.name,
                                      freq_threshold=0.01, Ne=1e5) {
     SLiM.outfile %>%
-    data.table::fread(header = F, sep = " ") %>%
-    ## Remove unnecessary columns from SLiM output. 
-    select(c("V2", "V5" , "V6", "V7", "V10", "V11", "V12")) %>%
-    rename(Generation = V2) %>%
-    rename(ID = V5) %>%
-    rename(Annotation = V6) %>%
-    rename(Position = V7) %>%
-    rename(Population = V10) %>%
-    rename(t0 = V11) %>%
-    rename(prevalence = V12) %>%
-    mutate(Annotation = recode(Annotation,
-                               m1 = "beneficial",
-                               m2 = "deleterious",
-                               m3 = "neutral",
-                               m4 = "background")) %>%
-    mutate(Population = recode(Population, p1 = pop.name)) %>%
-    mutate(Position = as.numeric(Position)) %>%
-    ## annotate the Gene for each mutation.
-    mutate(P1000 = trunc(Position/1000)+1) %>% 
-    mutate(Gene = paste("g", P1000, sep = "")) %>% 
-    select(-P1000) %>%
-    ## Convert prevalence to allele frequency.
-    ## The denominator is the number of individuals in the SliMulation. 
-    mutate("allele_freq" = prevalence/Ne) %>%
-    ## Filter mutations with allele frequencies above the sampling threshold.
-    filter(allele_freq > freq_threshold) %>%
-    arrange(ID, Generation) ## arrange each mutation by generation.
+        data.table::fread(header = F, sep = " ") %>%
+        ## Remove unnecessary columns from SLiM output. 
+        select(c("V2", "V5" , "V6", "V7", "V10", "V11", "V12")) %>%
+        rename(Generation = V2) %>%
+        rename(ID = V5) %>%
+        rename(Annotation = V6) %>%
+        rename(Position = V7) %>%
+        rename(Population = V10) %>%
+        rename(t0 = V11) %>%
+        rename(prevalence = V12) %>%
+        mutate(Annotation = recode(Annotation,
+                                   m1 = "beneficial",
+                                   m2 = "deleterious",
+                                   m3 = "neutral",
+                                   m4 = "background")) %>%
+        mutate(Population = recode(Population, p1 = pop.name)) %>%
+        mutate(Position = as.numeric(Position)) %>%
+        ## annotate the Gene for each mutation.
+        annotate.Gene.per.mutation() %>%
+        ## Convert prevalence to allele frequency.
+        ## The denominator is the number of individuals in the SliMulation. 
+        mutate("allele_freq" = prevalence/Ne) %>%
+        ## Filter mutations with allele frequencies above the sampling threshold.
+        filter(allele_freq > freq_threshold) %>%
+        arrange(ID, Generation) ## arrange each mutation by generation.
 }
 
 
